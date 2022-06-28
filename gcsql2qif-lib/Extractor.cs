865e8f8e31548bcd5af2686448a4d829b421e413
Jason Carter <jjcarter@me.com>
2022-06-28 17:31:03 +0100

    Logging mechanism is now loosely coupled

--- gcsql2qif-lib/Extractor.cs
+++ gcsql2qif-lib/Extractor.cs
@@ -1,52 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using GnuCash.Sql2Qif.Library.DAL;
 using GnuCash.Sql2Qif.Library.BLL;
 using System.Linq;
+using Microsoft.Extensions.Logging;
 
 namespace GnuCash.Sql2Qif.Library
 {
     public class Extractor
     {
-        public event EventHandler<LogEventArgs> LogEvent;
-        public IAccountDAO accDAO;
-        public ITransactionDAO trxDAO;
+        //public event EventHandler<LogEventArgs> LogEvent;
+        private readonly ILogger logger;
+        private readonly IAccountDAO accDAO;
+        private readonly ITransactionDAO trxDAO;
 
-
-        public Extractor(IAccountDAO accDAO, ITransactionDAO trxDAO)
+        public Extractor(ILogger<Extractor> logger, IAccountDAO accDAO, ITransactionDAO trxDAO)
         {
+            this.logger = logger;
             this.accDAO = accDAO;
             this.trxDAO = trxDAO;
         }
 
-        private void OnLogEvent(string level, string logMessage)
-        {
-            LogEventArgs args = new LogEventArgs()
-            {
-                LogLevel = level,
-                LogMessage = logMessage
-            };
-            LogEvent?.Invoke(this, args);
-        }
-
         public List<IAccount> ExtractData(string dataSource)
         {
-
-            OnLogEvent("INFO", "Extracting accounts...");
+            logger.LogInformation("Extracting accounts...");
             List<IAccount> accounts = accDAO.Extract(dataSource).ToList<IAccount>();
 
             // TODOO: Log some useful information like number of accounts, number of type of accounts
 
-            OnLogEvent("INFO", "Extracting transactions...");
+            logger.LogInformation("Extracting transactions...");
             List<ITransaction> transactions = trxDAO.Extract(dataSource, accounts).ToList<ITransaction>();
             // TODO: Log some useful information like number of transactions, number of transactions per account
 
             return accounts;
-        }
-
-        private void HandleLogEvent(object sender, LogEventArgs args)
-        {
-            OnLogEvent(args.LogLevel, args.LogMessage);
         }
     }
 }
